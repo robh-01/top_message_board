c7fb669d130acc2e9465304cf0a80405a1e906e2
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-03-26 16:29:30 +0545

    Change port for the deployment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use("/new", newMessageRouter);
 app.use("/messages", messageDetailsRouter);
 app.use("/", indexRouter);
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
